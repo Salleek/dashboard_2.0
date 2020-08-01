d9aa512bc7b26c3ee92fb71d5bc980f7cdf10884
Shaneil <shaneil.maharaj@yahoo.com>
2020-07-31 17:45:13 -0700

    try again at DTC page pt4

--- dashboard.py
+++ dashboard.py
@@ -1258,13 +1258,18 @@ def diag_display():
         screen.blit(no_error_msg, (340, 300))
 
     elif current_page == 6:
-        screen.blit(error_symbol_small, (465, 155))
-        # error_list = []
+        # screen.blit(error_symbol_small, (465, 155))
+        # error_num_list = []
+        # error_description_list = []
+        # # Separates tuples into two lists
         # for i in range(len(dtc_code)):
         #     error_num, error_description = dtc_code[i]
-        #     error_list.append(error_num)
-        error_label = error_font.render('Test', True, (255, 255, 255))
-        screen.blit(error_label, (465, 200))
+        #     error_num_list.append(error_num)
+        #     error_description_list.append(error_description)
+        # # Iterates through the two lists and prints list contents
+        # for j in range(len(dtc_code)):
+        #     error_label = error_font.render(error_num_list[j], True, (255, 255, 255))
+        #     screen.blit(error_label, (465, 200 + (j * 20)))
         clear_dtc_button()
 
 #Redraw
